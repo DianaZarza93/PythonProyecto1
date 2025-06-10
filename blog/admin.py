c2ff2cade4b8c80bbe48ce699d94831c5e635162
Diana Zarza <dianazarza088@gmail.com>
2025-06-10 20:18:16 -0300

    Registrar el modelo 'Post' en el admin con configuración personalizada para mejorar la visualización y filtrado de publicaciones.

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'estado', 'fecha_publicacion']
+    list_filter = ['estado', 'autor']
+    raw_id_fields = ['autor']
+    ordering = ['-fecha_publicacion']
