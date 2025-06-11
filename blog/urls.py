217a5968b1c553c02622372a970f23df7529e61f
Diana Zarza <dianazarza088@gmail.com>
2025-06-11 10:59:44 -0300

    Agregar formulario para crear publicaciones, mejorar la vista de lista de publicaciones y actualizar las URLs correspondientes.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('post/list', views.post_list, name='post_list'),
+    path('post/create', views.post_create, name='post_create'),
 ]
